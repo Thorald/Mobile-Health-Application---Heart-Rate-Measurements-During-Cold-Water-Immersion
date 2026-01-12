ea36437bf2778c2a3e0dc1f69301cc549e3efe8f
olivialiorbareket <s244668@dtu.dk>
2026-01-12 14:27:11 +0100

    Test - copy and paste :)

--- flutter_application/lib/view/dummy_startscreen.dart
+++ flutter_application/lib/view/dummy_startscreen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
   @override
 
   Widget build(BuildContext context) {
@@ -21,24 +21,71 @@ class MyApp extends StatelessWidget {
 }
 
 
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget({super.key});
 
-class MyTextWidget extends StatefulWidget {
- const MyTextWidget({Key? key}) : super(key: key);
- @override
-    State<MyTextWidget> createState() => _MyTextWidget();
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Welcome to Viking Bath!',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
   }
-class _MyTextWidget extends State<MyTextWidget> {
-    int count = 0;
-    @override
-
-    Widget build(BuildContext context){
-      return GestureDetector(
-        onTap: () {
-        setState((){
-        count++;
-      });
-    },
-    child: Center(child: Text('Click Me: $count')),
-  );
- }
 }
+
+
+
+ void main() => runApp(const MyApp());
+ class MyApp extends StatelessWidget {
+ const MyApp({Key? key}) : super(key: key);
+ static const String _title = 'SizedBox Widget Demo';
+ @override
+ Widget build(BuildContext context) {
+ return const MaterialApp(
+ title: _title,
+ home: MyStatelessWidget(),
+ );
+ }
+ }
+ class MyStatelessWidget extends StatelessWidget {
+ const MyStatelessWidget({Key? key}) : super(key: key);
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+ appBar: AppBar(title: const Text('SizedBox Example')),
+ body: _buildSizedBoxWidget(),
+ );
+ }
+ Widget _buildSizedBoxWidget() {
+ return SingleChildScrollView(
+ padding: const EdgeInsets.all(10.0),
+ child: Column(
+ children: const [
+ SizedBox(
+ width: 200,
+ height: 100,
+ child: Card(
+color: Colors.amber, 
+child: Center(child: Text('Developer'))),
+ ),
+ SizedBox(
+ width: 300,
+ height: 100,
+ child: Card(
+ color: Colors.green,
+ child: Center(child: Text('Flutter Framework'))),
+ ),
+ SizedBox(
+ width: 400,
+ height: 100,
+ child: Card(
+ color: Colors.blue, 
+child: Center(child: Text('Dart SDK'))),
+ ),
+ ],
+ ),
+ );
+ }
+ }
