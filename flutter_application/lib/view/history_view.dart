a6ac22b5465cab035e9e391312252fa26f21a1cd
olivialiorbareket <s244668@dtu.dk>
2026-01-16 15:14:02 +0100

    Changes 2

--- flutter_application/lib/view/history_view.dart
+++ flutter_application/lib/view/history_view.dart
@@ -13,16 +13,33 @@ class HistoryView extends StatelessWidget {
       ),
       body: ListView(
         padding: const EdgeInsets.all(16),
-        children: const [
+        children: [
         Text('Previous swims!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+
+        
 
         Image(image: AssetImage('assets/logo.png'), height: 50, width: 50),
         SizedBox(height: 12),
         ListTile(title: Text('Swim 1')),
         ListTile(title: Text('Swim 2')),
         ListTile(title: Text('Swim 3')),
-      ],
-    ),
+
+
+
+                    Positioned(
+                             top: 100,
+                             left: 0,
+                             child: Row(
+                               children: [Image.asset(
+                                   'assets/Bluetooth_icon.png',
+                                   width: 30,
+                                   height: 30,
+                                   fit: BoxFit.contain),
+        ],
+        ), 
+                    ),
+        ],
+      ),
 
       bottomNavigationBar: Container(
          height: 80,
